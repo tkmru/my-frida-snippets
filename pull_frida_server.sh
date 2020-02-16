969224a85e9c4f0a81dbd414ab0fc9cb8b14aa57
tkmru <i.am.tkmru@gmail.com>
2020-02-17 03:28:24 +0900

    add desc

--- pull_frida_server.sh
+++ pull_frida_server.sh
@@ -3,7 +3,7 @@
 wget -q -O - https://api.github.com/repos/frida/frida/releases \
 | jq '.[0] | .assets[] | select(.browser_download_url | match("frida-server-(.*)-android-arm64.xz")).browser_download_url' \
 | xargs wget -q --show-progress $1 && unxz frida-server*
-echo "Download has completed."
+echo "[+] Download has completed."
 
 adb push frida-server*-android-arm64 /data/local/tmp/
-echo "Frida server exists in /data/local/tmp/frida-server now"
+echo "[+] Frida server exists in /data/local/tmp/frida-server now"
