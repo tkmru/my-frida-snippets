37ca2f2959f55af1858f739e4b21064f1dad3a28
tkmru <i.am.tkmru@gmail.com>
2020-02-17 03:21:41 +0900

    add latest ARM64 frida-server download script

---
+++ pull_frida_server.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+wget -q -O - https://api.github.com/repos/frida/frida/releases \
+| jq '.[0] | .assets[] | select(.browser_download_url | match("frida-server-(.*)-android-arm64.xz")).browser_download_url' \
+| xargs wget -q --show-progress $1 && unxz frida-server*
+echo "Download has completed."
+
+adb push frida-server*-android-arm64 /data/local/tmp/
+echo "Frida server exists in /data/local/tmp/frida-server now"
